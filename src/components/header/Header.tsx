fbdc1887cf3f2e811c4072b02b55699aeaa7295c
Alexander Bazhenov alexbazhen@yandex.ru <alexbazhen@yandex.ru>
2022-08-26 20:43:41 +0300

    feat: code header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,24 +1,66 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
+import { clsx } from 'src/utils/clsx'
 import styles from './Header.module.scss'
 
 const Header: React.FC<any> = () => {
+  const isAuth = true; // TODO: подвязать к авторизации
+  const userName = 'Alexbazhen'; // TODO: подвязать к имени пользователя
+
   return (
-    <ul className={styles.header}>
-      <Link to='/' onClick={() => null}>
-        <li className={styles.headerItem}>Главная</li>
-      </Link>
-      <Link to='/textbook' onClick={() => null}>
-        <li className={styles.headerItem}>Учебник</li>
-      </Link>
-      <Link to='/games' onClick={() => null}>
-        <li className={styles.headerItem}>Games</li>
-      </Link>
-      <Link to='/login' onClick={() => null}>
-        <li className={styles.headerItem}>Login</li>
-      </Link>
-      <div className={styles.test}>SVG_TEST_BACKGROUDIMAGE</div>
-    </ul>
+    <header className={styles.header}>
+      <div className={globalThis.globalStyles.container}>
+        <div className={styles.headerWrapper}>
+          <Link to='/' className={clsx({
+            [styles.headerLogo]: true,
+            ['_icon-logo']: true
+          })} onClick={() => null}>
+          </Link>
+          <nav className={styles.headerNav}>
+            <ul className={styles.headerList}>              
+              <li className={styles.headerItem}>
+                <NavLink to='/textbook' className={({ isActive }) => 
+                  isActive ? styles.itemActive : ''
+                } onClick={() => null}>Учебник</NavLink>
+              </li>
+              <li className={styles.headerItem}>
+                <NavLink to='/games' className={({ isActive }) => 
+                  isActive ? styles.itemActive : ''
+                } onClick={() => null}>Мини-игры</NavLink>
+              </li>
+              <li className={styles.headerItem}>
+                <NavLink to='/stat' className={({ isActive }) => 
+                  isActive ? styles.itemActive : ''
+                } onClick={() => null}>Статистика</NavLink>
+              </li>
+              {/* <Link to='/login' onClick={() => null}>
+                <li className={styles.headerItem}>Login</li>
+              </Link>
+              <div className={styles.test}>SVG_TEST_BACKGROUDIMAGE</div>  */}
+            </ul>
+          </nav>
+          {!isAuth ?
+            <div className={styles.headerAuth}>
+              <button className={styles.authLogin}>Вход</button>
+              <button className={styles.authRegister}>Регистрация</button>              
+            </div>
+            :
+            <button className={clsx({
+              [styles.headerUsername]: true,
+              ['_icon-settings']: true
+            })}>{userName}</button>
+          }
+          <button className={clsx({
+            [styles.headerUser]: true,
+            ['_icon-user']: !isAuth,
+            ['_icon-settings']: isAuth
+          })}></button> 
+          <button className={styles.headerBurger}>
+            <span></span>
+          </button>
+        </div>
+      </div>
+    </header>
   )
 }
 
