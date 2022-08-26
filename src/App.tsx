240ac9c1b29987e0dbbed9666ea4070b25d9dbf0
Alexander Bazhenov alexbazhen@yandex.ru <alexbazhen@yandex.ru>
2022-08-26 20:07:57 +0300

    feat: add scss variables, add mixins.scss in global styles, small edits in structure styles

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,8 @@ import styles from './App.module.scss'
 import TextBookPage from './components/textbookPage/TextbookPage'
 import RegistrationPage from './components/auth/RegistrationPage'
 
+globalThis.globalStyles = styles;
+
 function App() {
   return (
     <BrowserRouter>
