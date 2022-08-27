63faa7519d4c2973bddf79e5e37db4c9e720a38c
unknown <tegmcgregor@zohomail.com>
2022-08-27 23:25:45 +0300

    feat: add main page component markup

--- src/components/mainPage/MainPage.tsx
+++ src/components/mainPage/MainPage.tsx
@@ -1,45 +1,23 @@
 import React, { useEffect, useState } from 'react'
 import styles from './MainPage.module.scss'
-import Button from '../common/button/Button'
-import Loader from '../common/loader/Loader'
-import IconButton from '../common/icon-button/IconButton'
-import iconTest from '../../assets/test/logo.svg'
-import ArrowUp from '../../assets/test/ArrowUp.svg'
-import { clsx } from '../../utils/clsx'
-import { stlx } from '../../utils/stlx'
+
+// import { stlx } from '../../utils/stlx'
+import { Link } from 'react-router-dom'
+import { SplashComponent } from './mainComponents/SplashComponent';
+import { LegendComponent } from './mainComponents/LegendComponent';
+import { VideoComponent } from './mainComponents/VideoComponent';
+// import wave from '../../assets/svg/wave.svg'
+// import enggo from '../../assets/svg/enggo.svg'
 
 const MainPage: React.FC<any> = () => {
-  const [color, setColor] = useState('')
-  const [value, setValue] = useState(0)
-
-  // const a = useAppSelector(state => state.)
-  useEffect(() => {
-    console.log('valueeffect', value)
-    setValue((prev) => prev + 1)
-  }, [])
-
-  const buttonHandler = () => {
-    if (color === 'white') setColor('blue')
-    else setColor('white')
-  }
-
-  console.log('value', value)
+  const isAuth = false;
 
   return (
-    <div
-      className={clsx({
-        [styles.mainPageWrapper]: true,
-      })}
-      style={stlx({
-        [`background: ${color};`]: true,
-      })}
-    >
-      value{value}
-      <Button classes={styles.exampleButton} text={'buttonForTest'} onClick={buttonHandler} />
-      <Loader />
-      <IconButton icon={iconTest} height={'height: 12px'} width={'width: 12px'} disabled={false} />
-      <IconButton icon={ArrowUp} height={'height: 12px'} width={'width: 12px'} disabled={false} />
-    </div>
+    <>
+    <SplashComponent auth={isAuth} />
+    <LegendComponent  auth={isAuth} />
+    {/* <VideoComponent /> */}
+    </>
   )
 }
 
---
+++ src/components/mainPage/mainComponents/SplashComponent.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import styles from './SplashComponent.scss'
+import { clsx } from '../../../utils/clsx'
+import { Link } from 'react-router-dom'
+
+type PropsType = {
+  auth: boolean
+}
+
+export function SplashComponent ({ auth }:PropsType) {
+
+  return (
+    <section className={styles.mainSection}>
+      <div className={globalThis.globalStyles.container}>
+      <div className={styles.mainSectionBg}>
+        <img src='./assets/svg/wave.svg' alt="" />
+      </div>
+        <div className={clsx({[styles.mainSectionWrapper]: true})}>
+          <div className={styles.introText}>
+            <h1 className={styles.introTextHeader}>
+             Устал откладывать английский на потом?            
+            </h1>
+            <h2 className={styles.introTextSubheader}>
+             Попробуй учить слова с Enggo!
+            </h2>
+            <p className={styles.introTextContent}>
+            Ты не успеешь заметить, как выучишь почти 4000 английских слов, занимаясь всего по 20 минут* в день
+            </p>
+            <blockquote className={styles.introTextQuote}>
+            * - минимально рекомендуемая норма времени ежедневного обучения в Enggo
+            </blockquote>
+            <div className={styles.introActions}>
+              <button className={styles.introActionsGo}>EngGo!</button>
+              <Link to='/textbook' className={clsx({
+                [styles.introActionsMore]: true
+                })} onClick={() => null}>
+                  Узнать больше об EngGo
+              </Link>
+            </div>
+          {auth &&
+            <h1 className={styles.introTextHeader}>
+              For authorized            
+            </h1>
+          }
+          </div>
+          <div className={styles.introDecoration}>
+            <div className={styles.introDecorationText}>
+              <span className={styles.introDecorationTextLine}>#ENGLISH</span>
+              <span className={styles.introDecorationTextLine}>&amp;LOVE</span>
+            </div>
+            <div className={styles.introDecorationDog}>
+              <img src='./assets/svg/enggo.svg' alt="ENGGO" />
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </section>
+  )
+}
