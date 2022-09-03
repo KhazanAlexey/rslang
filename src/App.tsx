a57cf9f5bbcc72580cc1bad4a1471c4fc3f53302
Alexander Bazhenov <alexbazhen@yandex.ru>
2022-09-03 23:28:39 +0300

    fix: remove links to register page and login page in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import LoginPage from './components/auth/LoginPage'
 import { MainPage } from './components/mainPage/MainPage'
 import Footer from './components/footer/Footer'
 import Header from './components/header/Header'
 import GamesPage from './components/gamesPage/GamesPage'
 import styles from './App.module.scss'
 import TextBookPage from './components/textbookPage/TextbookPage'
-import RegistrationPage from './components/auth/RegistrationPage'
 import StatPage from './components/statPage/StatPage'
 import AudioCall from './components/games/audioCall/AudioCall'
 import Sprint from './components/games/sprint/Sprint'
@@ -22,12 +20,10 @@ export function App() {
 
         <Routes>
           <Route path='/' element={<MainPage />} />
-          <Route path='/login' element={<LoginPage />} />
           <Route path='/games' element={<GamesPage />} />
           <Route path='/games/audiocall' element={<AudioCall />} />
           <Route path='/games/sprint' element={<Sprint />} />
           <Route path='/textbook' element={<TextBookPage />} />
-          <Route path='/registration' element={<RegistrationPage />} />
           <Route path='/stat' element={<StatPage />} />
         </Routes>
 
