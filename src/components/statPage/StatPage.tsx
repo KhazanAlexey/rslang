1e9f6428d9db78eb1dcb992eae7174e2a177f82e
Alexander Bazhenov <alexbazhen@yandex.ru>
2022-09-07 17:55:19 +0300

    feat: add template for stat page

--- src/components/statPage/StatPage.tsx
+++ src/components/statPage/StatPage.tsx
@@ -1,7 +1,17 @@
 import React from 'react'
+import styles from './StatPage.module.scss'
 
 const StatPage: React.FC<any> = () => {
-  return <h1>Статистика</h1>
+  return (
+    <div className={styles.stat}>
+      <section className={styles.statInfo}>
+        <div className={globalThis.globalStyles.container}>
+          <h1 className={globalThis.globalStyles.pageTitle}>Статистика</h1>
+          <p className={styles.statDescr}>Скоро тут будет страница статистики</p>
+        </div>
+      </section>
+    </div>
+  )
 }
 
 export default StatPage
