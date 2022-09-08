2bacb0fd6c3bb1963ab576f5b2d4fc25362646c8
Alexander Bazhenov <alexbazhen@yandex.ru>
2022-09-08 17:46:48 +0300

    fix: add some statistic in statpage

--- src/components/statPage/StatPage.tsx
+++ src/components/statPage/StatPage.tsx
@@ -14,6 +14,8 @@ const StatPage: React.FC<any> = () => {
   // optional: {attempts: 1, successAttempts: 1}
   // wordId: "5e9f5ee35eb9e72bc21af5bf"
 
+  const isStat = userWords.find((word) => (word.optional?.attempts ?? 0) > 0)
+
   const allAttempts = userWords.reduce(
     (sum, currentWord) => sum + (currentWord.optional?.attempts ?? 0),
     0,
@@ -23,6 +25,10 @@ const StatPage: React.FC<any> = () => {
     0,
   )
 
+  const allNewWords = userWords.reduce(
+    (sum, currentWord) => sum + ((currentWord.optional?.attempts ?? 0) > 0 ? 1 : 0),
+    0,
+  )
   const allPercentRight = Math.floor(allSuccessAttempts / (allAttempts / 100))
 
   // TODO: ВОЗМОЖНО НЕПРАВИЛЬНАЯ ПРОВЕРКА НА ПУСТОЙ USERWORDS
@@ -32,19 +38,21 @@ const StatPage: React.FC<any> = () => {
         <div className={globalThis.globalStyles.container}>
           <h1 className={globalThis.globalStyles.pageTitle}>Статистика</h1>
           <p className={styles.statDescr}>Скоро тут будет страница статистики</p>
-          {userWords && (
+          {isStat && (
             <div>
-              <ul>
+              <h2>За все время</h2>
+              {/* <ul>
                 {userWords.map((word, ind) => (
                   <p key={ind}>
-                    Слово: {word.wordId}, попыток: {word.optional?.attempts}, успешных:{' '}
+                    Слово: {word.wordId}, попыток: {word.optional?.attempts ?? 0}, успешных:{' '}
                     {word.optional?.successAttempts ?? '0'}
                   </p>
                 ))}
-              </ul>
+              </ul>*/}
+              <p>Новых слов в играх: {allNewWords}</p>
               <p>Всего попыток со словами: {allAttempts}</p>
               <p>Из них успешных: {allSuccessAttempts}</p>
-              <p>Правильных: {allPercentRight}%</p>
+              <p>Процент правильных ответов в играх: {allPercentRight}%</p>
             </div>
           )}
         </div>
